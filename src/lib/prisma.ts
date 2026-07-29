4a2a34f8d008d62481fb54ec4a79e78e943daa55
laraibahmad <laraib.ahmad@techtics.ai>
2026-07-29 21:08:21 +0500

    deveolpment issue fox

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,18 @@
-import { PrismaClient } from "@prisma/client";
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { PrismaClient } = require("@prisma/client") as typeof import("@prisma/client/index");
 import { PrismaPg } from "@prisma/adapter-pg";
 
-function createPrismaClient() {
+type PrismaClientType = InstanceType<typeof PrismaClient>;
+
+function createPrismaClient(): PrismaClientType {
   const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL!,
   });
-  return new PrismaClient({ adapter });
+  return new PrismaClient({ adapter }) as PrismaClientType;
 }
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma: PrismaClientType | undefined;
 };
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
